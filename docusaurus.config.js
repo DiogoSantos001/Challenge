e2ce49f964398a311c0510234c2be9ed7f2bd74b
DiogoSantos001 <diogo.fs@live.com.pt>
2022-08-18 03:08:57 +0100

    website

---
+++ docusaurus.config.js
@@ -0,0 +1,67 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+
+const config = {
+  title: 'My Info Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    ({
+      navbar: {
+        title: 'Diogo Santos',
+        logo: {
+          alt: 'foto_curriculo.jpg',
+          src: 'img/foto_curriculo.jpg',
+        },
+        items: [
+         
+          {to: '/CV', label: 'CV', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Diogo Santos Built with Docusaurus.`,
+      },
+      prism: {
+      },
+    }),
+};
+
+module.exports = config;
